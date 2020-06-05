27bf8d072d925eb301e2e5c6705434f4b9771141
nprindle <nprindle18@gmail.com>
2020-06-05 17:17:00 -0400

    Use faster checks for powers of 2

--- src/Data/Primitive/Contiguous/FFT.hs
+++ src/Data/Primitive/Contiguous/FFT.hs
@@ -1,5 +1,4 @@
 {-# language BangPatterns        #-}
-{-# language LambdaCase          #-}
 {-# language NoImplicitPrelude   #-}
 {-# language ScopedTypeVariables #-}
 
@@ -18,8 +17,8 @@ import Control.Applicative (pure)
 import Control.Monad (when,unless)
 import Control.Monad.Primitive (PrimMonad(..))
 import Control.Monad.ST (runST)
-import Data.Bits (shiftR,shiftL,(.&.),(.|.))
-import Data.Bool (Bool,otherwise)
+import Data.Bits (shiftR,shiftL,(.&.))
+import Data.Bool (Bool,(&&))
 import Data.Complex (Complex(..),conjugate)
 import Data.Eq (Eq(..))
 import Data.Function (($),(.))
@@ -73,9 +72,8 @@ arrOK :: forall arr a. (Contiguous arr, Element arr a)
   => arr a
   -> Bool
 {-# inline arrOK #-}
-arrOK arr =
-  let n = Contiguous.size arr
-  in (1 `shiftL` log2 n) == n
+arrOK arr = (n > 0) && (n .&. n - 1 == 0)
+  where n = Contiguous.size arr
 
 -- | Radix-2 decimation-in-time fast Fourier Transform.
 --   The given array must have a length that is a power of two,
@@ -94,7 +92,7 @@ mfft mut = do
               then inner (k `shiftR` 1) (l - k)
               else bitReverse (i + 1) (l + k)
         inner (len `shiftR` 1) j
-    stage l l1 = unless (l == (log2 len)) $ do
+    stage l l1 = unless (1 `shiftL` l == len) $ do
       let
         !l2 = l1 `shiftL` 1
         !e = (negate twoPi) / (intToDouble l2)
@@ -118,25 +116,6 @@ mfft mut = do
       flight 0 0
   bitReverse 0 0
 
--- wildcard cases should never happen. if they do, really bad things will happen.
-b,s :: Int -> Int
-b = \case { 0 -> 0x02; 1 -> 0x0c; 2 -> 0xf0; 3 -> 0xff00; 4 -> wordToInt 0xffff0000; 5 -> wordToInt 0xffffffff00000000; _ -> 0; }
-s = \case { 0 -> 1; 1 -> 2; 2 -> 4; 3 -> 8; 4 -> 16; 5 -> 32; _ -> 0; }
-{-# inline b #-}
-{-# inline s #-}
-
-log2 :: Int -> Int
-log2 v0 = if v0 <= 0
-  then Prelude.error $ "Data.Primitive.Contiguous.FFT: nonpositive input, got " Prelude.++ Prelude.show v0
-  else go 5 0 v0
-  where
-    go !i !r !v
-      | i == -1 = r
-      | v .&. b i /= 0 =
-          let si = s i
-          in go (i - 1) (r .|. si) (v `shiftR` si)
-      | otherwise = go (i - 1) r v
-
 swap :: forall arr m x. (PrimMonad m, Contiguous arr, Element arr x)
   => Mutable arr (PrimState m) x
   -> Int
@@ -156,10 +135,6 @@ twoPi = 6.283185307179586
 intToDouble :: Int -> Double
 {-# inline intToDouble #-}
 intToDouble = Prelude.fromIntegral
-
-wordToInt :: Word -> Int
-{-# inline wordToInt #-}
-wordToInt = Prelude.fromIntegral
 
 intToComplexDouble :: Int -> Complex Double
 {-# inline intToComplexDouble #-}
